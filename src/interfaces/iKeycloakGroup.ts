79845c8cc9a712d425e6a28b9b5804bd8eb34aff
ReebaPaysys <reeba.siddiqui@paysyslabs.com>
2026-03-02 09:46:56 +0500

    fix: resolve coderabbit comment

--- src/interfaces/iKeycloakGroup.ts
+++ src/interfaces/iKeycloakGroup.ts
@@ -1,31 +1,31 @@
 export interface KeycloakGroupAccess {
-  view: boolean;
-  viewMembers: boolean;
-  manageMembers: boolean;
-  manage: boolean;
-  manageMembership: boolean;
+  view?: boolean;
+  viewMembers?: boolean;
+  manageMembers?: boolean;
+  manage?: boolean;
+  manageMembership?: boolean;
 }
 
 export interface GroupAttribute {
-  TENANT_ID: string[];
+  TENANT_ID?: string[];
+  [key: string]: string[] | undefined;
 }
 
 export interface KeycloakGroup {
-  id: string;
-  name: string;
-  description: string;
-  path: string;
-  subGroupCount: number;
-  subGroups: KeycloakSubGroup[];
-  access: KeycloakGroupAccess;
-  attributes: GroupAttribute;
+  id?: string;
+  name?: string;
+  description?: string;
+  path?: string;
+  subGroupCount?: number;
+  subGroups?: KeycloakSubGroup[];
+  access?: KeycloakGroupAccess;
+  attributes?: GroupAttribute;
 }
 
 export interface KeycloakSubGroup extends KeycloakGroup {
-  parentId: string;
-  attributes: GroupAttribute;
-  realmRoles: string[];
-  clientRoles: Record<string, string[]>;
+  parentId?: string;
+  realmRoles?: string[];
+  clientRoles?: Record<string, string[]>;
 }
 
 export interface KeycloakGroupMember {
